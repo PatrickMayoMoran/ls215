5f725ae14d0b03b06eeb7fa05348137a6686582b
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-01-11 15:14:20 +0000

    Add alternate solution to every problem that avoids traversing entire list

--- list_processing_and_functional_abstractions/every.js
+++ list_processing_and_functional_abstractions/every.js
@@ -1,8 +1,18 @@
-function myOwnEvery(array, func) {
+function myOwnEveryOption(array, func) {
   array.forEach((element, index, array) => {
     let falsy = !(func(element, index, array));
     if (falsy) return false;
   });
+  return true;
+}
+
+function myOwnEvery(array, func) {
+  for (let i = 0; i < array.length; i += 1) {
+    let element = array[i];
+    let falsy = !func(element, i, array);
+    if (falsy) return false;
+  }
+
   return true;
 }
 
