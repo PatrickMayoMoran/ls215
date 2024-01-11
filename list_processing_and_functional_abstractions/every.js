29c82d77b57283393f40f5e014e38e13a1d3ecf3
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-01-11 15:11:31 +0000

    Add solution to myEvery exercise

--- list_processing_and_functional_abstractions/every.js
+++ list_processing_and_functional_abstractions/every.js
@@ -1,6 +1,11 @@
 function myOwnEvery(array, func) {
-  // ...
+  array.forEach((element, index, array) => {
+    let falsy = !(func(element, index, array));
+    if (falsy) return false;
+  });
+  return true;
 }
 
 let isAString = value => typeof value === 'string';
-myOwnEvery(['a', 'a234', '1abc'], isAString);       // true
+console.log(myOwnEvery(['a', 'a234', '1abc'], isAString));       // true
+console.log(myOwnEvery([], isAString)); // true for empty arrays/vacuous truths
