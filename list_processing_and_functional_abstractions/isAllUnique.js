6eebd0388ac4c11891967b63035487b61a30923f
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-01-15 08:24:10 +0000

    Add isAllUnique low level problem solution

--- list_processing_and_functional_abstractions/isAllUnique.js
+++ list_processing_and_functional_abstractions/isAllUnique.js
@@ -1,10 +1,26 @@
 function isAllUnique(string) {
-  // ...
+  let seen = {};
+  let lowerString = string.toLowerCase();
+
+  for (let i = 0; i < lowerString.length; i += 1) {
+    let character = lowerString[i];
+    if (character === ' ') {
+      continue;
+    }
+
+    if (seen[character]) {
+      return false;
+    } else {
+      seen[character] = true;
+    }
+  }
+
+  return true;
 }
 
-isAllUnique('The quick brown fox jumped over a lazy dog');  // false
-isAllUnique('123,456,789');                                 // false
-isAllUnique('The big apple');                               // false
-isAllUnique('The big apPlE');                               // false
-isAllUnique('!@#$%^&*()');                                  // true
-isAllUnique('abcdefghijklmnopqrstuvwxyz');                  // true
+console.log(isAllUnique('The quick brown fox jumped over a lazy dog'));  // false
+console.log(isAllUnique('123,456,789'));                                 // false
+console.log(isAllUnique('The big apple'));                               // false
+console.log(isAllUnique('The big apPlE'));                               // false
+console.log(isAllUnique('!@#$%^&*()'));                                  // true
+console.log(isAllUnique('abcdefghijklmnopqrstuvwxyz'));                  // true
