9a82887311c3c5c9944a64859b86cd51468f092f
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-01-15 23:24:57 +0000

    Add loop iteration for building reverse string

---
+++ string_and_text_processing/reverse.js
@@ -0,0 +1,11 @@
+function reverse(string) {
+  let reversedString = '';
+  for (let i = string.length - 1; i >= 0; i -= 1) {
+    reversedString += string[i];
+  }
+
+  return reversedString;
+}
+
+console.log(reverse('hello'));                  // returns "olleh"
+console.log(reverse('The quick brown fox'));    // returns "xof nworb kciuq ehT"
