c5463a3c3bff6159df9af135cae718c68b21643e
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-01-15 23:35:57 +0000

    Add alternative solution to acronym problem

--- string_and_text_processing/acronym.js
+++ string_and_text_processing/acronym.js
@@ -3,8 +3,19 @@ function acronym(string) {
   return words.reduce((acronym, word) => acronym + word[0].toUpperCase(), '');
 }
 
+function otherAcronym(string) {
+  let normalized = string.replaceAll(/-/g, ' ').split(' ');
+  let upperChars = normalized.map(word => word[0].toUpperCase());
+  return upperChars.join('');
+}
+
 console.log(acronym('Portable Network Graphics'));                  // "PNG"
 console.log(acronym('First In, First Out'));                        // "FIFO"
 console.log(acronym('PHP: HyperText Preprocessor'));                // "PHP"
 console.log(acronym('Complementary metal-oxide semiconductor'));    // "CMOS"
 console.log(acronym('Hyper-text Markup Language'));                 // "HTML"
+console.log(otherAcronym('Portable Network Graphics'));                  // "PNG"
+console.log(otherAcronym('First In, First Out'));                        // "FIFO"
+console.log(otherAcronym('PHP: HyperText Preprocessor'));                // "PHP"
+console.log(otherAcronym('Complementary metal-oxide semiconductor'));    // "CMOS"
+console.log(otherAcronym('Hyper-text Markup Language'));                 // "HTML"
