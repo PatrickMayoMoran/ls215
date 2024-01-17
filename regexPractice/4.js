c3dd1ee1079ffd2be698fdd3ec4c3f0790456765
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-01-17 18:06:13 +0000

    Add solution to Regex practice problem 4

---
+++ regexPractice/4.js
@@ -0,0 +1,7 @@
+function mysteriousMath(equation) {
+  let operators = /[-+*\/]/g
+  return equation.replaceAll(operators, '?');
+}
+
+console.log(mysteriousMath('4 + 3 - 5 = 2'));           // -> '4 ? 3 ? 5 = 2'
+console.log(mysteriousMath('(4 * 3 + 2) / 7 - 1 = 1')); // -> '(4 ? 3 ? 2) ? 7 ? 1 = 1'
