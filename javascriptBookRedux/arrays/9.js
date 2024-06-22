184dd7e29473a6245df8ec05f2fe6cfb0210c74d
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-06-22 14:17:59 -0400

    Add alternate solution to includes 3 problem using filter and length

--- javascriptBookRedux/arrays/9.js
+++ javascriptBookRedux/arrays/9.js
@@ -4,6 +4,15 @@ let numbers1 = [1, 3, 5, 7, 9, 11];
 let numbers2 = [];
 let numbers3 = [2, 4, 6, 8];
 
-console.log(numbers1.includes(3));
-console.log(numbers2.includes(3));
-console.log(numbers3.includes(3));
+function includes3(array) {
+  let onlyThrees = array.filter(n => n === 3);
+  if (onlyThrees.length > 0 ) return true;
+  return false;
+}
+
+console.log(includes3(numbers1));
+console.log(includes3(numbers2));
+console.log(includes3(numbers3));
+// console.log(numbers1.includes(3));
+// console.log(numbers2.includes(3));
+// console.log(numbers3.includes(3));
