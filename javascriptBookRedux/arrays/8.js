5102b48e4609b12ca63f9f53f4dd2ee97c5f87c7
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-06-22 14:06:10 -0400

    Add reduce solution to problem 8

--- javascriptBookRedux/arrays/8.js
+++ javascriptBookRedux/arrays/8.js
@@ -1,2 +1,13 @@
+// Write a function similar to the oddLengths function from Exercise 6, but don't use map or filter. Instead, try to use the reduce method.
+// reduce
+//  empty array as accumulator
+//  if length of element is odd, push to acc
+function oddLengths(arr) {
+  return arr.reduce((acc, e) => {
+    if (e.length % 2 !== 0) acc.push(e.length);
+    return acc;
+  }, []);
+}
+
 let arr = ['a', 'abcd', 'abcde', 'abc', 'ab'];
 console.log(oddLengths(arr)); // => [1, 5, 3]
