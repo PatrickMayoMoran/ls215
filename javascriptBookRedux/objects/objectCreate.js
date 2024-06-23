3df35ae8eceaa28e960d0a0893e4d6c0712acd2a
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-06-23 12:05:46 -0400

    Add for... in loop showing iteration over inherited and own keys

--- javascriptBookRedux/objects/objectCreate.js
+++ javascriptBookRedux/objects/objectCreate.js
@@ -2,5 +2,7 @@ let bob = {name: "Bob", age: 22};
 let studentBob = Object.create(bob);
 studentBob.year = 'Senior';
 
-console.log(studentBob.name);
-console.log(studentBob);
+console.log(Object.keys(studentBob)); // will only be 'year'
+for (let key in studentBob) {         // will show us name, age, and year
+  console.log(`Key of ${key} and value of ${studentBob[key]}`);
+}
