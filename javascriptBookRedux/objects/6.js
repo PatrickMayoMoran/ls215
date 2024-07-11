541da14d1bdab9e57a983f9aa9a83ea8bd0e5609
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-07-11 12:49:23 -0400

    Add solution for problem 6

--- javascriptBookRedux/objects/6.js
+++ javascriptBookRedux/objects/6.js
@@ -7,3 +7,14 @@ let myProtoObj = {
 let myObj = Object.create(myProtoObj);
 myObj.qux = 3;
 console.log(myObj);
+
+// Will only show keys on object itself, so qux
+let objKeys = Object.keys(myObj);
+objKeys.forEach(function(key) {
+  console.log(key);
+});
+
+// Will show keys on object first - qux - then inherited - foo and bar
+for (let key in myObj) {
+  console.log(key);
+}
