430f378a78a3a057c94b0a12fe39ad842d2466fb
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-07-11 14:07:56 -0400

    Add initial solution to exercise 6 using match

---
+++ javascriptBookRedux/moreStuff/6.js
@@ -0,0 +1,14 @@
+function allMatches(words, regEx) {
+  return words.filter(word => word.match(regEx));
+}
+
+let words = [
+  'laboratory',
+  'experiment',
+  'flab',
+  'Pans Labyrinth',
+  'elaborate',
+  'polar bear',
+];
+
+console.log(allMatches(words, /lab/)); // => ['laboratory', 'flab', 'elaborate']
