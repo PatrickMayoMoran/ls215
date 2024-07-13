c954f0e4af1463d1b4e4fa7780a6c52ec34eed1e
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-07-13 16:07:01 -0400

    Redo solution for problem 6

---
+++ javascriptBookRedux/moreStuff/redo6.js
@@ -0,0 +1,14 @@
+function allMatches(words, regEx) {
+  return words.filter(word => regEx.test(word));
+}
+
+let words = [
+  'laboratory',
+  'experiment',
+  'flab',
+  'Pans Labyrinth',
+  'elaborate',
+  'polar bear',
+];
+
+console.log(allMatches(words, /lab/)); // => ['laboratory', 'flab', 'elaborate']
