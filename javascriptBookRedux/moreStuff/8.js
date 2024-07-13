680619a2147440913cd4e63ea2d98ebba306c589
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-07-13 16:20:56 -0400

    Add first attempt for exercise 8 of more stuff

---
+++ javascriptBookRedux/moreStuff/8.js
@@ -0,0 +1,23 @@
+// write a function that returns true if argument is NaN, false otherwise
+
+// NaN:
+//  - typeof should return string 'number'
+//  - value is not === to itself
+function isNotANumber(value) {
+  if (typeof value !== "number") {
+    return false;
+  } else if (value !== value) {
+    return true;
+  }
+}
+
+console.log(isNotANumber(NaN)); // true, all others false
+console.log(isNotANumber(7));
+console.log(isNotANumber('meow'));
+console.log(isNotANumber(4.56));
+console.log(isNotANumber(Infinity));
+console.log(isNotANumber(-Infinity));
+console.log(isNotANumber(0));
+console.log(isNotANumber(true));
+console.log(isNotANumber(undefined));
+console.log(isNotANumber(null));
