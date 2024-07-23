3ce7739cbb78100b93ca34c625cc4b80d1fe65b3
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-07-23 19:34:35 -0400

    Add example of Object.entries showing symbols converted to strings

---
+++ fluency/object/entries.js
@@ -0,0 +1,4 @@
+// Object.entries
+// returns a nested array of key value pairs of an object's properties
+let letters = { a: 1, b: 2, c: 3 };
+console.log(Object.entries(letters)); // [['a', 1], ['b', 2], ['c', 3]]
