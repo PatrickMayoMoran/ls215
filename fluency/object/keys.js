67e51219042f8c5ebc6aaeff1ac69407b5fdb943
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-07-23 20:04:31 -0400

    Add example of using Object.keys to get array of keys from an object

---
+++ fluency/object/keys.js
@@ -0,0 +1,8 @@
+// Object.keys
+// returns an array of all the keys in an object
+// useful when you need to iterate through the keys of an object
+
+let test = { a: 1, b: 2, c: 3 };
+let empty = {};
+console.log(Object.keys(test));
+console.log(Object.keys(empty));
