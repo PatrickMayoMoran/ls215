bf200b7cf00fee13a1eb5eaf3d58a0ef4de66eb7
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-07-24 18:31:13 -0400

    Add solution to madLibs problem using browser prompt for user input

---
+++ exercises/easy_2/madLibs.js
@@ -0,0 +1,6 @@
+let noun = prompt("Enter a noun: ");
+let verb = prompt("Enter a verb: ");
+let adverb = prompt("Enter an adverb: ");
+let adjective = prompt("Enter an adjective: ");
+
+console.log(`Do you ${verb} your ${adjective} ${noun} ${adverb}? That's hilarious!`);
