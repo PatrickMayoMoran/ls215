17bbda589df37f11cad87bcf138798457f6aa914
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-07-25 15:08:05 -0400

    Add solution to century problem

---
+++ exercises/easy_2/century.js
@@ -0,0 +1,39 @@
+function century (year) {
+  let century = Math.floor(year / 100) + 1;
+
+  if (year % 100 === 0) {
+    century -= 1;
+  }
+
+  if (catchWithTh(century)) {
+    return String(century) + 'th';
+  }
+
+  return String(century) + centurySuffix(century)
+}
+
+const catchWithTh = (lastTwo) => lastTwo === 11 || lastTwo === 12 || lastTwo === 13;
+
+function centurySuffix(century) {
+  if (catchWithTh(century % 100)) {
+    return 'th';
+  }
+
+  let lastDigit = century % 10;
+  switch (lastDigit) {
+    case 1: return 'st'
+    case 2: return 'nd'
+    case 3: return 'rd'
+    default: return 'th'
+  }
+}
+
+console.log(century(2000));        // "20th"
+console.log(century(2001));        // "21st"
+console.log(century(1965));        // "20th"
+console.log(century(256));         // "3rd"
+console.log(century(5));           // "1st"
+console.log(century(10103));       // "102nd"
+console.log(century(1052));        // "11th"
+console.log(century(1127));        // "12th"
+console.log(century(11201));       // "113th"
