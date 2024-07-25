c56a5f7c069d962375464ca78091ce05c2f9e4cc
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-07-25 15:54:46 -0400

    Add solution using Date object to get current year rather than hard coding

--- exercises/easy_3/retire.js
+++ exercises/easy_3/retire.js
@@ -2,7 +2,9 @@ function whenRetire () {
   let age = parseInt(prompt("How old are you ? "));
   let retirementAge = parseInt(prompt("At what age would you like to retire? "));
   let workingYears = retirementAge - age;
-  let year = 2024;
+
+  let date = new Date()
+  let year = date.getFullYear();
 
   console.log(`It is ${year}. You will retire in ${workingYears + year}`);
   console.log(`You only have ${workingYears} left to go!`);
