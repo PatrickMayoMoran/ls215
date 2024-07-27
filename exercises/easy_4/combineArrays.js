ed4d67ced0cca691c6658b64288cf890ca6b49ea
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-07-27 16:14:37 -0400

    Add general combine Array solution using rest parameters

--- exercises/easy_4/combineArrays.js
+++ exercises/easy_4/combineArrays.js
@@ -1,8 +1,17 @@
-function union (arrayOne, arrayTwo) {
-  arrayTwo.forEach(e => {
-    if (!arrayOne.includes(e)) arrayOne.push(e);
+function union (...args) {
+  let resultArray = [];
+
+  args.forEach(array => copyNonDups(resultArray, array));
+
+  return resultArray;
+}
+
+function copyNonDups (resultArray, array) {
+  array.forEach(e => {
+    if (!resultArray.includes(e)) resultArray.push(e);
   });
 
-  return arrayOne;
+  return resultArray;
 }
+
 console.log(union([1, 3, 5], [3, 6, 9]));    // [1, 3, 5, 6, 9]
