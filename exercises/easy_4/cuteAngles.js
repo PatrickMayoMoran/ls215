b1fdd01a49d0718a9d030e89b70ed63b42e3d10b
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-07-27 15:54:48 -0400

    Add solution to cute Angles problem using parseInt and padStart

--- exercises/easy_4/cuteAngles.js
+++ exercises/easy_4/cuteAngles.js
@@ -1,4 +1,37 @@
+// GOAL: given a floating point number between 0 and 360 inclusive, return 
+// a string representing it in degrees, minutes, and seconds
+
+// INPUT: floating point number
+// OUTPUT: string
+
+// RULES:
+//  Always returning minutes and seconds with two digits
+//  will always be given floating point number between 0 and 360
+//  rounding method does not matter
+
+// DATA STRUCTURES
+//  
+// ALGORITHM
+//  - store degrees, then calculate minutes
+//  - store minutes, then calculate seconds
+//
+//  degrees is integer value of starting number
+//  minutes is the remainder of starting number mod 1 times 60
+//  minutes value is integer value of minutes
+//  seconds is the remainder of minutes mod 1 times 60
+//  seconds gets rounded to 2 decimal places
+//  string is degrees, degree symbol, minutes, seconds
+
 const DEGREE = '\u00B0';
+
+function dms(number) {
+  let degrees = parseInt(number);
+  let minutesValue = number % 1 * 60;
+  let minutes = String(parseInt(minutesValue)).padStart(2, 0);
+  let secondsValue = minutesValue % 1 * 60;
+  let seconds = String(parseInt(secondsValue)).padStart(2, 0);
+  return `${degrees}${DEGREE}${minutes}'${seconds}"`
+}
 
 // All test cases should return true
 console.log(dms(30) === "30°00'00\"");
