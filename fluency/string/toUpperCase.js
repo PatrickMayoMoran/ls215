3d5703102642d0f063fb2470f800dd5c6c35cdb2
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-21 19:29:35 -0400

    Add examples of using toUpperCase

---
+++ fluency/string/toUpperCase.js
@@ -0,0 +1,14 @@
+/*
+* toUpperCase converts the entire string to upper case
+*/
+
+let meow = "meow";
+let numbers = "one1two2three3";
+let other = "****meow****woof$$$$chaos!!!";
+
+console.log(meow);
+console.log(meow.toUpperCase());
+console.log(numbers);
+console.log(numbers.toUpperCase());
+console.log(other);
+console.log(other.toUpperCase());
