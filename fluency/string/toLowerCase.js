c9f4bcdd1c3ff4738422a46c1349eb62127fc586
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-21 19:34:31 -0400

    Add examples using toLowerCase to convert strings to all lower case

---
+++ fluency/string/toLowerCase.js
@@ -0,0 +1,11 @@
+/*
+* toLowerCase returns a new string with all the characters in lowercase
+*/
+
+let sentence = "Santa Cruz is the capital of Santa Cruz County in California.";
+let scream = "WHY ARE WE SCREAMING?";
+
+console.log(sentence);
+console.log(sentence.toLowerCase());
+console.log(scream);
+console.log(scream.toLowerCase());
