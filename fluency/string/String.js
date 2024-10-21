c291b44f38bd2bf5e1f75eea45abe2628dae944c
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-21 19:56:29 -0400

    Add example showing String() handling Symbol

--- fluency/string/String.js
+++ fluency/string/String.js
@@ -9,3 +9,4 @@ console.log(String(55));
 console.log(String(55) instanceof Object); // false, not a string object
 console.log(String(undefined)); // String can handle undefined, unlike toString
 console.log(String(null)); // String can handle null, unlike toString
+console.log(String(Symbol("Was ist am passieren?")));
