587a85527cfb396c6d9b1b004347455ddaf2f93c
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-21 19:45:13 -0400

    Add examples of using toString and String for string conversion, showing difference when used with literals

---
+++ fluency/string/toString.js
@@ -0,0 +1,11 @@
+/*
+*  toString returns the string value of the object it is called on
+*  toString does not work on literals; you need to use String for those
+*/
+
+let stringy = "Switzerland";
+console.log("Meow".toString());
+console.log(stringy.toString());
+console.log((55).toString());
+console.log(String(undefined));
+console.log(String(55));
