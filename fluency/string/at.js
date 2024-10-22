35d2601ae56c533c27a36822025737eb201f97c2
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-21 20:17:03 -0400

    Add examples showing accessing characters with at and undefined for out of bounds

---
+++ fluency/string/at.js
@@ -0,0 +1,13 @@
+/*
+    at returns the character at the specified index
+    at accepts negative indices
+    if index is out of bounds, returns undefined
+*/
+
+let tio = 'tio';
+
+console.log(tio.at(0));
+console.log(tio.at(-2));
+console.log(tio.at(2));
+console.log(tio.at(3));
+console.log(tio.at(-4));
