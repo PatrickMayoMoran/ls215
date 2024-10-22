f0eebdb130dc056ac0fc7025c51ded4ebf8a86d3
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-21 20:46:54 -0400

    Add example showing using includes to see if a given string contains the search string

---
+++ fluency/string/includes.js
@@ -0,0 +1,8 @@
+/*
+    includes returns true or false for if the search string is present
+*/
+
+let tio = 'tio';
+console.log(tio.includes('tio'));
+console.log(tio.includes('')); // empty string returns true
+console.log(tio.includes('meow'));
