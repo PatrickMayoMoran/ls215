4d61a459cd6720e5fc88bec86ce5310fbf004011
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-21 20:25:20 -0400

    Add example showing charCode at returning number for character at index or NaN for out of bounds index

---
+++ fluency/string/charCodeAt.js
@@ -0,0 +1,8 @@
+/*
+    charCodeAt returns the UTF-16 numerical value of the character at the given index
+    If the index is out of bounds, NaN is returned
+*/
+
+let tio = 'tio';
+console.log(tio.charCodeAt(0));
+console.log(tio.charCodeAt(-3));
