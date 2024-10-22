20aa80c067d0b2d2a511ccab937bd6ece8cd724c
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-21 20:12:24 -0400

    Add examples showing accessing the length property on string objects and wrapped string primitives

---
+++ fluency/string/length.js
@@ -0,0 +1,8 @@
+/*
+    length is property of string objects that corresponds to the number of
+    characters.
+*/
+
+let tio = new String("tio");
+console.log("Meow".length);
+console.log(tio.length);
