efd4c1bcc54649153508a47a4985e2c45e42ffbd
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-22 18:19:35 -0400

    Add comments showing results of example output

--- fluency/string/endsWith.js
+++ fluency/string/endsWith.js
@@ -5,7 +5,7 @@
 */
 
 let tio = 'tio';
-console.log(tio.endsWith('o'));
-console.log(tio.endsWith('tio')); // search strings can be long!
-console.log(tio.endsWith('meow'));
-console.log((tio.endsWith('')));
+console.log(tio.endsWith('o'));   // true
+console.log(tio.endsWith('tio')); // true - search strings can be long!
+console.log(tio.endsWith('meow'));// false
+console.log((tio.endsWith('')));  // true - empty string returns true
