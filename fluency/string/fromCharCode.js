ebc9363187dce61bb05ae26b9712a1b2cfc1239e
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-23 10:17:01 -0400

    Add comments showing expected output for fromCharCode examples

--- fluency/string/fromCharCode.js
+++ fluency/string/fromCharCode.js
@@ -2,5 +2,5 @@
 *   fromCharCode generates a sequence of characters based on the codepoint given
 */
 
-console.log(String.fromCharCode(65, 66, 67));
-console.log(String.fromCharCode(0x2014));
+console.log(String.fromCharCode(65, 66, 67)); // ABC
+console.log(String.fromCharCode(0x2014));     // -
