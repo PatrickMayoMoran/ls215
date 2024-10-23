9b5fd2db631aff985ff8043484389d9cf087359d
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-23 10:19:39 -0400

    Add comments showing expected output for toLowerCase examples

--- fluency/string/toLowerCase.js
+++ fluency/string/toLowerCase.js
@@ -5,7 +5,7 @@
 let sentence = "Santa Cruz is the capital of Santa Cruz County in California.";
 let scream = "WHY ARE WE SCREAMING?";
 
-console.log(sentence);
-console.log(sentence.toLowerCase());
-console.log(scream);
-console.log(scream.toLowerCase());
+console.log(sentence); // "Santa Cruz is the capital of Santa Cruz County in California."
+console.log(sentence.toLowerCase()); // "santa cruz is the capital of santa cruz county in california"
+console.log(scream); // "WHY ARE WE SCREAMING?"
+console.log(scream.toLowerCase()); // "why are we screaming?"
