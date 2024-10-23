3efddd6ded7034d0db96559e73c67933a15e0be9
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-23 13:37:58 -0400

    Add examples playing around with usage and syntax of if statement

---
+++ fluency/string/ifTesting.js
@@ -0,0 +1,10 @@
+if (('tio'.indexOf('cat')) < 0) console.log("Mayo Understands");
+
+let result;
+
+function returnMayo() {
+  if (('tio'.indexOf('cat')) < 0) return "Mayo Understands";
+}
+
+result = returnMayo();
+console.log(result);
