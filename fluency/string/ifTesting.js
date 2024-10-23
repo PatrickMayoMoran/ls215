03fc8c9a34e7d2b8a38d7b33c541883eb78676ec
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-23 13:42:55 -0400

    Add comments showing how if statement cannot be used in variable initialization

--- fluency/string/ifTesting.js
+++ fluency/string/ifTesting.js
@@ -9,5 +9,10 @@ function returnMayo() {
 result = returnMayo();
 console.log(result);
 
+
+//let noThankYou = if (('tio'.indexOf('cat')) < 0) console.log("Mayo Understands");
+// Above does NOT work because if is a statement, not expression - it does not
+// evaluate to a value so cannot be used to initialize a variable
+
 let expressionNotStatement = ("tio".indexOf('cat') < 0) ? "Success!" : "Failure";
-console.log(expressionNotStatement);
+console.log(expressionNotStatement); // ternary expression does evaluate so can be used in variable initialization
