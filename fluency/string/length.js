0a73157e415941576df1a1e6cca701f3d2e28e43
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-23 10:28:10 -0400

    Add comments showing expected output for length examples

--- fluency/string/length.js
+++ fluency/string/length.js
@@ -4,5 +4,6 @@
 */
 
 let tio = new String("tio");
-console.log("Meow".length);
-console.log(tio.length);
+console.log("Meow".length); // 4
+console.log(tio.length); // 3
+console.log((55).length); // undefined - number has no length property to read
