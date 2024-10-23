90e6b9ac2d132ff6d6fd659a2012886dda78b468
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-23 10:13:29 -0400

    Add comments showing expected output for String examples

--- fluency/string/String.js
+++ fluency/string/String.js
@@ -4,9 +4,9 @@
   can be used as a constructor to create objects
 */
 
-console.log(String("meow"));
-console.log(String(55));
+console.log(String("meow")); // "meow"
+console.log(String(55));     // "55"
 console.log(String(55) instanceof Object); // false, not a string object
 console.log(String(undefined)); // String can handle undefined, unlike toString
 console.log(String(null)); // String can handle null, unlike toString
-console.log(String(Symbol("Was ist am passieren?")));
+console.log(String(Symbol("Was ist am passieren?"))); // String can handle symbols
