03eb6ec8a8c713321f68af9028ba56a9d97b216d
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-23 13:44:58 -0400

    Add comments describing single line if statements with and without return

--- fluency/string/ifTesting.js
+++ fluency/string/ifTesting.js
@@ -1,7 +1,9 @@
+// if statement one line to have some code execute
 if (('tio'.indexOf('cat')) < 0) console.log("Mayo Understands");
 
 let result;
 
+// if statement with single line return within a function
 function returnMayo() {
   if (('tio'.indexOf('cat')) < 0) return "Mayo Understands";
 }
@@ -9,8 +11,7 @@ function returnMayo() {
 result = returnMayo();
 console.log(result);
 
-
-//let noThankYou = if (('tio'.indexOf('cat')) < 0) console.log("Mayo Understands");
+// let noThankYou = if (('tio'.indexOf('cat')) < 0) console.log("Mayo Understands");
 // Above does NOT work because if is a statement, not expression - it does not
 // evaluate to a value so cannot be used to initialize a variable
 
