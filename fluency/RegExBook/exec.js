5d20c5edbcb2c8ec9e6903c220774b91e424849b
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-24 16:54:23 -0400

    Add example showing exec returning first match even when multiple matches are present

--- fluency/RegExBook/exec.js
+++ fluency/RegExBook/exec.js
@@ -6,7 +6,9 @@
 let pattern = /santa/;
 let aMatch = 'santa cruz is beautiful'
 let noMatch = 'farwell is beautiful'
+let manyMatches = 'santa cruz, santa clara, and santa barbara are all cities';
 
 console.log(pattern.exec(aMatch)); // ['santa', index: 0, input: 'santa cruz is beautiful, groups: undefined]
 console.log(pattern.exec(noMatch)); // null
 console.log(pattern.exec(aMatch).index); // 0
+console.log(pattern.exec(manyMatches)); // ['santa', index: 0,...]
