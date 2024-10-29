509bc7ea4ebd1dd6296e63b89a793717fcd97934
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-29 15:45:46 -0400

    Add example using matchAll with global flag to get iterable of all matches with info

---
+++ fluency/string/matchAll.js
@@ -0,0 +1,13 @@
+/*
+  matchAll returns an iterable of nested arrays with info on all matches
+  matchAll requires the global flag otherwise a TypeError is thrown
+*/
+
+let regex = /santa/ig;
+let sentence = "Santa Cruz, Santa Clara and Santa Barbara are all cities.";
+
+sentence.matchAll(regex).forEach(match => console.log(match));
+// spread syntax practice
+let matches = [...sentence.matchAll(regex)];
+console.log(matches[0]);
+console.log(matches[1]);
