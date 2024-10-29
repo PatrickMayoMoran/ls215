003e5789635afe85ac9d71fe88b3453771f61cfb
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-29 15:56:47 -0400

    Add examples of padding strings with padStart

---
+++ fluency/string/padStart.js
@@ -0,0 +1,9 @@
+/*
+  padStart will add padding to the beginning of string
+  default is space, can specify
+  if padding length is less than string length, same string is returned
+*/
+
+console.log("Meow".padStart(15));
+console.log("Meow".padStart(15, '-'));
+console.log("Meow".padStart(2, '-'));
