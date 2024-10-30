d3e3144e96397d34599d8b818567ce42525d85f3
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-30 17:00:05 -0400

    Add examples showing use of repeat function

---
+++ fluency/string/repeat.js
@@ -0,0 +1,9 @@
+/*
+  repeat is called on a string and takes a count of how many times to repeat
+  returns a new string
+*/
+
+let tiny = "Tiny Cat";
+console.log(tiny.repeat(0));
+console.log(tiny.repeat(1));
+console.log(tiny.repeat(2));
