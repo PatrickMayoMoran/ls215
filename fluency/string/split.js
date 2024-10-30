d0a5071a7c4dd57d2876052699598ed6ea4c7622
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-30 17:34:52 -0400

    Add example of returning empty array by giving a limit of 0 results for result array

--- fluency/string/split.js
+++ fluency/string/split.js
@@ -9,3 +9,4 @@ console.log(cat.split()); // ['tiny cat']
 console.log(cat.split('')); // ['t', 'i', 'n', ...]
 console.log(cat.split(' ')); // ['tiny', 'cat'] - separator not included
 console.log(cat.split(/y/, 1)); // ['tin'] - limit on how many results
+console.log(cat.split(/y/, 0)); // []
