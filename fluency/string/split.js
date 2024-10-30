ef939e8b96c9903c92b94eed8a758e43f8048e4a
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-30 17:34:17 -0400

    Add examples of using split with different separators and limits

---
+++ fluency/string/split.js
@@ -0,0 +1,11 @@
+/*
+  split returns an array of results based on the designated separator
+  no separator means whole string
+  empty string separator means every character
+*/
+
+let cat = 'tiny cat';
+console.log(cat.split()); // ['tiny cat'] 
+console.log(cat.split('')); // ['t', 'i', 'n', ...]
+console.log(cat.split(' ')); // ['tiny', 'cat'] - separator not included
+console.log(cat.split(/y/, 1)); // ['tin'] - limit on how many results
