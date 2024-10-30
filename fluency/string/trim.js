78a41ebabcb8958baafdf354db8c5a2bd8a1eb6b
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-10-30 18:11:39 -0400

    Add examples showing how to use trim family to trim whitespace from start and end of strings

---
+++ fluency/string/trim.js
@@ -0,0 +1,8 @@
+/*
+  the trim family trims white space from the beginning, end, or both
+*/
+
+let cat = '      tiny cat      ';
+console.log(cat.trim());
+console.log(cat.trimStart());
+console.log(cat.trimEnd());
