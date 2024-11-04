1869c2f5e9c3527d10329cca0fead53a45093d1d
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-04 12:08:16 -0800

    Add solution to extended swapName problem to account for multiple first names

--- exercises/easy_5/swapName.js
+++ exercises/easy_5/swapName.js
@@ -1,5 +1,16 @@
+/*
+** Original solution for two name names
 function swapName(name) {
   return name.split(' ').reverse().join(', ');
 }
+*/
+
+function swapName(name) {
+  // let lastName = / \w+$/; -- this removes last name
+  let names = name.split(' ');
+  let lastName = names.pop();
+  return lastName + ', ' + names.join(' ');
+}
 
 console.log(swapName('Joe Roberts'));    // "Roberts, Joe"
+console.log(swapName('John Jacob Jingleheimer Schmidt'));
