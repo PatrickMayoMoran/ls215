65a116c522bd08b94990b64343e89a9b5d2123a8
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-04 11:37:52 -0800

    Add original solution for always returning a negative number problem

---
+++ exercises/easy_5/negative.js
@@ -0,0 +1,8 @@
+function negative(number) {
+  if (number >= 0) return -number;
+  return number;
+}
+
+console.log(negative(5));     // -5
+console.log(negative(-3));    // -3
+console.log(negative(0));     // -0
