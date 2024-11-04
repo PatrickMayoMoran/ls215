8327825033a5c277708d70b17ac93528e46c2c66
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-04 11:32:46 -0800

    Add provided solution using substring and clearer center and left index variable designations

--- exercises/easy_5/centerOf.js
+++ exercises/easy_5/centerOf.js
@@ -1,8 +1,20 @@
+/*
 function centerOf(string) {
   if (isOdd(string.length)) {
     return string[Math.floor(string.length/2)];
   } else {
     return string.substring((string.length/2) - 1,(string.length/2) + 1);
+  }
+}
+*/
+
+function centerOf(string) {
+  if (isOdd(string.length)) {
+    let centerIndex = Math.floor(string.length / 2);
+    return string[centerIndex];
+  } else {
+    let leftIndex = (string.length / 2) - 1;
+    return string.substring(leftIndex, leftIndex + 2);
   }
 }
 
