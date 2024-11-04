94de650b55ee867044469ff35b7e8d9b7b891da1
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-04 08:50:57 -0800

    Add arrow function syntax practice to array average problem

--- exercises/easy_4/arrayAverage.js
+++ exercises/easy_4/arrayAverage.js
@@ -1,8 +1,9 @@
 function average(array) {
-  let sum = array.reduce(function (a, c) {
+  let sum = array.reduce((a, c) => { // arrow function syntax
+//let sum = array.reduce( function (a,c) { ---- anonymous function syntax
     return a + c
   }, 0);
-  console.log(Math.max(...array));
+  // console.log(Math.max(...array)); - practicing Math.max with unpacked array
   return Math.floor(sum / array.length);
 }
 
