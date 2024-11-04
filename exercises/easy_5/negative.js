460dac5a5bbb42e9702f005a3dd8fa88dd757fc3
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-04 11:40:03 -0800

    Add provided solution to always returning a negative number using Math.abs * -1

--- exercises/easy_5/negative.js
+++ exercises/easy_5/negative.js
@@ -1,7 +1,17 @@
+/*
+** Original solution
 function negative(number) {
   if (number >= 0) return -number;
   return number;
 }
+*/
+
+/*
+** Provided solution
+function negative(number) {
+  return Math.abs(number) * -1;
+}
+*/
 
 console.log(negative(5));     // -5
 console.log(negative(-3));    // -3
