936e5a1d658bee7e111259d75be1e3ae1cb27692
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-06 10:46:49 -0800

    Add example of passing function as argument to another function that expects a callback

---
+++ redo/listProcessingAndFunctionalAbstractions/3FunctionsAsArguments.js
@@ -0,0 +1,32 @@
+"use strict"
+/*
+  Take advantage of first class functions by:
+    creating a function iterate through an array
+    Have that function take a callback function as an argument
+
+  Being able to pass functions as arguments means we don't need to hardcode
+    functionality into our iterate method.
+    The only thing locked into implementation is how our array is 
+    iterated through.
+    Flexible is what array is passed and what the callback function does.
+*/
+
+function iterate(array, callback) {
+  for (let i = 0; i < array.length; i += 1) {
+    callback(array[i]);
+  }
+}
+
+// Our array
+let count = [1, 2, 3, 4, 5];
+
+// Callback
+function logger(element) {
+  console.log(element);
+}
+
+// invocation without first class function stored in variable
+iterate(count, function (number) { console.log(number)});
+
+// invocation with first class function in variable
+iterate(count, logger);
