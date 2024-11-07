69274f4eea66dfecaecac6e74f4e5a555750a4c7
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-06 18:06:21 -0800

    Add example of using myReduce implementation for reduce functionality

--- redo/listProcessingAndFunctionalAbstractions/myReduce.js
+++ redo/listProcessingAndFunctionalAbstractions/myReduce.js
@@ -35,3 +35,14 @@ let sum = (result, value) => result + value;
 
 console.log(myReduce([5, 12, 15, 1, 6], smallest));           // 1
 console.log(myReduce([5, 12, 15, 1, 6], sum, 10));            // 49
+
+// EXAMPLE
+function longestWord(words) {
+  return myReduce(words, longest);
+}
+
+function longest(result, currentWord) {
+  return currentWord.length >= result.length ? currentWord : result;
+}
+
+console.log(longestWord(['abc', 'launch', 'targets', '']));      // "targets"
