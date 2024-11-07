3309c08dc6e4c71570b79ca7b605dc0c8cf103c2
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-06 17:59:30 -0800

    Add buggy implementation for myReduce that checks for initial value, but does not update index accordingly

---
+++ redo/listProcessingAndFunctionalAbstractions/myReduce.js
@@ -0,0 +1,20 @@
+/*
+BUGGY IMPLEMENTATION - incase of no initial value given,
+  the first value of the array will be used twice - once as the initial value
+  and once again as the first element in the array
+  Need to amend implementation to start iteration on 0 or 1 index based
+  on initial value
+function myReduce(array, func, initial) {
+  initial = initial || array[0];
+  let result = initial;
+  array.forEach(e => result = func(result, e));
+
+  return result;
+}
+*/
+
+let smallest = (result, value) => (result <= value ? result : value);
+let sum = (result, value) => result + value;
+
+console.log(myReduce([5, 12, 15, 1, 6], smallest));           // 1
+console.log(myReduce([5, 12, 15, 1, 6], sum, 10));            // 49
