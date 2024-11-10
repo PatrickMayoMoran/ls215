9de996c76d5c6ee2c7ad9bb6018a7395f2a33b60
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-10 09:43:57 -0800

    Add solution to fruit grocery problem

--- redo/exercises/buyFruit.js
+++ redo/exercises/buyFruit.js
@@ -1,3 +1,4 @@
+/*
 function buyFruit(list) {
   return list.reduce((groceryList, item) => {
     let quantity = item[1];
@@ -7,6 +8,24 @@ function buyFruit(list) {
     }
     return groceryList;
   }, []);
+}
+*/
+
+function buyFruit(fruitsList) {
+  return fruitsList.map(item => repeat(item))
+            .reduce((grocery, fruit) => grocery.concat(fruit));
+}
+
+function repeat(item) {
+  let quantity = item[1];
+  let fruit = item[0];
+  let result = [];
+
+  for (let i = 0; i < quantity; i += 1) {
+    result.push(fruit);
+  }
+
+  return result;
 }
 console.log(buyFruit([['apple', 3], ['orange', 1], ['banana', 2]]));
 // returns ["apple", "apple", "apple", "orange", "banana", "banana"
