82f8aeea5f013d5c9a240f96e68f09aac4297388
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-10 12:48:54 -0800

    Add RegEx replace solution to removeVowels problem

---
+++ redo/stringProcessing/removeVowels.js
@@ -0,0 +1,6 @@
+function removeVowels(words) {
+  return words.map(w => w.replace(/[aeiou]/gi, ''));
+}
+console.log(removeVowels(['abcdefghijklmnopqrstuvwxyz']));         // ["bcdfghjklmnpqrstvwxyz"]
+console.log(removeVowels(['green', 'YELLOW', 'black', 'white']));  // ["grn", "YLLW", "blck", "wht"]
+console.log(removeVowels(['ABC', 'AEIOU', 'XYZ']));                // ["BC", "", "XYZ"]
