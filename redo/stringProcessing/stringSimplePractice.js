350b5f389a5205bcd3e2e5fd22aa3fe757943858
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-11-10 11:36:12 -0800

    Add simple practice prompts for string methods

---
+++ redo/stringProcessing/stringSimplePractice.js
@@ -0,0 +1,78 @@
+/*
+// 1
+let firstName = "Patrick";
+let lastName = "Moran";
+let fullName = [firstName, lastName].join(' ');
+console.log(fullName);
+
+// 2
+console.log(firstName.concat(lastName));
+
+// 3
+let names = fullName.split(' ');
+console.log(names);
+
+// 4
+let language = 'JavaScript';
+let idx = language.indexOf('S');
+console.log(idx);
+
+// 5
+let charCode = language.charCodeAt(idx);
+console.log(charCode);
+
+// 6
+console.log(String.fromCharCode(charCode));
+
+// 7
+console.log(language.lastIndexOf('a'));
+
+// 8
+let a = 'a';
+let b = 'b';
+console.log(a > b);
+b = 'B';
+console.log(a > b);
+
+// 9
+console.log(language.slice(1,4));
+console.log(language.slice(2,4));
+
+// 10
+console.log(language.substring(1,4));
+console.log(language.substring(2,4));
+
+// 11
+console.log(language.slice(1, -1));
+console.log(language.slice(2, -1));
+
+// 12
+console.log(language.substring(1, -1));
+console.log(language.substring(2, -1));
+
+// 13
+let fact1 = 'JavaScript is fun';
+let fact2 = 'Kids like it too';
+let compoundSentence = fact1 + ' and ' + fact2.toLowerCase();
+console.log(compoundSentence);
+
+// 14
+console.log(fact1[0], fact2[0]);
+
+// 15
+let pi = 22/7;
+console.log(pi.toString().lastIndexOf('14'));
+
+// 16
+// let boxNumber = 356.toString(); - REJECTED because period looks like decimal
+// let boxNumber = 356..toString(); - ACCEPTED but bad style
+let boxNumber = (356).toString(); // ACCEPTED and considered good style
+console.log(boxNumber);
+
+// 17
+boxNumber = parseInt(boxNumber);
+console.log(typeof boxNumber);
+boxNumber = String(boxNumber);
+console.log(typeof boxNumber);
+*/
+
